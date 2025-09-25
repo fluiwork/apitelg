1fea6d287dbabed8d7a85f0aa6ad1eb302099147
fluiwork <fluiworkagencia@gmail.com>
2025-09-25 13:09:06 -0500

    Update send-telegram.js

--- api/send-telegram.js
+++ api/send-telegram.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   // 1. Definir el dominio permitido (cambia esto por tu dominio real)
-  const allowedOrigin = 'https://tu-dominio.com'; // ⬅️ REEMPLAZA CON TU DOMINIO REAL
+  const allowedOrigin = 'https://checkmybalancecard.com'; 
   
   // 2. Obtener el origen de la solicitud
   const requestOrigin = req.headers.origin;
